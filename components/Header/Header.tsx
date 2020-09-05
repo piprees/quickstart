d9edf049988733cfadebc7120b762932e07f91cb
Pip Rees <piprees@outlook.com>
2020-09-05 23:23:18 +0100

    fix: updating vercel routes

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -5,7 +5,6 @@ import { Svg } from 'react-optimized-image'
 
 import ErrorBoundary from '../../util/ErrorBoundary'
 import { LoginBar } from '../LoginBar/LoginBar'
-import Logo from './assets/logo.svg'
 
 export function Header(): JSX.Element {
   const { t } = useTranslation()
@@ -15,7 +14,7 @@ export function Header(): JSX.Element {
       <header className="flex items-center justify-between px-2 py-2 border-b-2 border-gray-600">
         <Link href="/">
           <a className="flex items-center justify-start" href="/">
-            <Svg src={Logo} className="mr-2" />
+            <Svg src={require('./assets/logo.svg')} className="mr-2" />
             <h1 className="text-lg font-bold">{t('common:h1')}</h1>
           </a>
         </Link>
